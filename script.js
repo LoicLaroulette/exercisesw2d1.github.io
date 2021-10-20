00d72a2f9f5502e1674372dabd4e415e856a85c9
LoicLaroulette <larouletteloic05@gmail.com>
2021-10-20 16:09:34 +0400

    exercisesw2d1

---
+++ script.js
@@ -0,0 +1,51 @@
+let num1 = 5;
+let num2 = "34"
+console.log (num1 + num2)
+
+let num3 = 5;
+let num4 = "4"
+console.log (num3 - num4)
+
+let num5 = 10;
+let num6 = 5
+console.log (num5 % num6)
+
+let num7 = 5;
+let num8 = 10
+console.log (num7 % num8)
+
+let text1 = "Java";
+let text2 = "Script"
+console.log (text1 + text2)
+
+let blank1 = " ";
+let blank2 = " "
+console.log (blank1 + blank2)
+
+let blank3 = " ";
+let num9 = 0
+console.log (blank3 + num9)
+
+let text3 = "true";
+let text4 = "true"
+console.log (text3 + text4)
+
+let text5 = "true";
+let text6 = "false"
+console.log (text5 + text6)
+
+let text7 = "false";
+let text8 = "true"
+console.log (text7 + text8)
+
+let text9 = "false";
+let text10 = "true"
+console.log (text9 - text10)
+
+let num10 = 3;
+let num11 = 4
+console.log (num10 + num11)
+
+let text12 = "Bob";
+let text13 = "Bill"
+console.log (text12 - text13)
